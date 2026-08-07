077500d6e696cd38240f27ee86eb1df5d51caca7
david <davidakabueze067@gmail.com>
2026-08-07 16:01:40 +0100

    did search bar

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import SearchBar from "./components/SearchBar";
+import SearchLocation from "./components/SearchLocation";
+import CatergoriesDisplay from "./components/CatergoriesDisplay";
+
 
 const App = () => {
   return (
     <>
       <div className="min-h-screen bg-[#f5f7f3]">
-        <div className="mx-5 overflow-hidden">
+        <div className="mx-5">
           <Header />
           <Hero />
-          <SearchBar />
+          <SearchLocation />
+          <CatergoriesDisplay/>
         </div>
       </div>
     </>
